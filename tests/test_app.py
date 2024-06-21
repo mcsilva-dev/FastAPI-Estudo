74e241c8eb7bf1f2bec7a3d1610394bc598b4368
miguel <miguelcamposdasilva@gmail.com>
2024-06-21 14:22:29 -0300

    Adicionado os testes para exceptions e metodo get_user

--- tests/test_app.py
+++ tests/test_app.py
@@ -50,6 +50,36 @@ def test_read_users(client):
     }
 
 
+def test_get_user(client):
+    response = client.get('/users/1')
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'username': 'alice',
+        'email': 'alice@example.com',
+        'id': 1,
+    }
+
+
+def test_get_user_exception(client):
+    response = client.get('/users/2')
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found'}
+
+
+def test_update_user_exception(client):
+    response = client.put(
+        '/users/2',
+        json={
+            'username': 'miguel',
+            'email': 'miguel@teste.com',
+            'password': 'newpassword',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found'}
+
+
 def test_update_users(client):
     response = client.put(
         '/users/1',
@@ -66,6 +96,12 @@ def test_update_users(client):
         'email': 'miguel@teste.com',
         'id': 1,
     }
+
+
+def test_delete_users_exception(client):
+    response = client.delete('/users/2')
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found'}
 
 
 def test_delete_users(client):
