eb86078216a10d080bab703f189f29994b0793ff
miguel <miguelcamposdasilva@gmail.com>
2024-06-26 10:25:13 -0300

    Novos testes

--- fast_zero/models.py
+++ fast_zero/models.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
-from sqlalchemy import func, Column, String, Integer, DateTime
-from sqlalchemy.orm import Mapped, registry, mapped_column
+from sqlalchemy import func
+from sqlalchemy.orm import Mapped, mapped_column, registry
 
 table_registry = registry()
 
@@ -10,12 +10,13 @@ table_registry = registry()
 class User:
     __tablename__ = 'users'
 
-    id: Mapped[int] = mapped_column(
-        init=False, primary_key=True
-    )
+    id: Mapped[int] = mapped_column(init=False, primary_key=True)
     username: Mapped[str] = mapped_column(init=False, unique=True)
     password: Mapped[str] = mapped_column(nullable=False)
     email: Mapped[str] = mapped_column(unique=True)
     created_at: Mapped[datetime] = mapped_column(
         init=False, server_default=func.now()
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        init=False, server_default=func.now(), onupdate=func.now()
     )
