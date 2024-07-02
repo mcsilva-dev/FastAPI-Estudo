cf93808e439d357eaa2a205b17b6fdbb728efd97
miguel <miguelcamposdasilva@gmail.com>
2024-07-02 00:02:32 -0300

    seguindo a aula

--- migrations/env.py
+++ migrations/env.py
@@ -42,6 +42,7 @@ def run_migrations_offline() -> None:
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
         compare_type=True,
+        render_as_batch=True,
     )
 
     with context.begin_transaction():
@@ -66,6 +67,7 @@ def run_migrations_online() -> None:
             connection=connection, 
             target_metadata=target_metadata,
             compare_type=True,
+            render_as_batch=True
         )
 
         with context.begin_transaction():
